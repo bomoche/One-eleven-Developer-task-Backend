649a24f3182772eef43e54a3e11b7952f83eae4e
Bongani Moche <bonganimoche8@gmail.com>
2026-03-06 12:15:21 +0200

    Added cors for network requests

--- one-developer-api-task/src/app.js
+++ one-developer-api-task/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const sortRoute = require("./routes/alphabeticalSorting");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
